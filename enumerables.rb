ee6dacda7ff99e145611bbc591faaea567a5c52e
ciminilloa <ciminilloa@findlay.edu>
2021-11-08 18:02:19 -0500

    2nd commit

--- enumerables.rb
+++ enumerables.rb
@@ -37,10 +37,36 @@ class Array
     self.my_each { |ele| return true if prc.call(ele) }
     false
   end
+
+  def my_all?(&prc)
+    self.my_each { |ele| return false if !prc.call(ele) }
+    true
+  end
+
+  def my_flatten
+
+    if !self.is_a?(Array)
+      return [self]
+    end
+
+    new_array = []
+    self.my_each do |ele|
+      new_array += ele.my_flatten
+    end
+    return new_array
+
+  end
+
+
+
+
 end
 
-arr = [1, 2, 3]
+# arr = [1, 2, 3]
 # arr.my_each { |el| p el }
 # p arr.my_select { |el| el.even? }
 # p arr.my_reject { |el| el.even? }
 # p arr.my_any? { |ele| ele.even? }
+# p arr.my_all? { |ele| ele.even? }
+
+[1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]

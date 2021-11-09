b5998154eb8cf7eb9c81082067183a62e279fd5b
ciminilloa <ciminilloa@findlay.edu>
2021-11-08 20:20:57 -0500

    game.rb

--- ghost/game.rb
+++ ghost/game.rb
@@ -1,10 +1,34 @@
 
 
 
+class Game
+
+    def initialize
+        @player_1 = 
+        @fragment = ""
+        path = "dictionary.txt"
+        text = File.read(path)
+        text_array = text.split("\n")
+        dictionary_hash = Hash.new
+        text_array.each { |word| dictionary_hash[word] = word }
+        @dictionary = dictionary_hash
+    end
+
+    def play_round
+
+
+    end
 
 
 
 
+
+
+
+end
+
+
+g = Game.new
 
 
 

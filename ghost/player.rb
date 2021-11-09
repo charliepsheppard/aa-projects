c2587ef4747973764610ab0280fe1e9fb2f0d1de
Charlie Sheppard <sheppard.charlie24@gmail.com>
2021-11-08 17:33:25 -0800

    Add up to player

--- ghost/player.rb
+++ ghost/player.rb
@@ -1,6 +1,23 @@
 
+class Player
+  def initialize(name)
+    @name = name
+  end
 
+  def guess
+    response = gets.chomp
+  end
 
+  def alert_invalid_guess(string)
+    alpha = ("a".."z").to_a
+    if !alpha.include?(string)
+      raise "Invalid guess"
+    end
+  end
+end
+
+p = Player.new("Charlie")
+p.alert_invalid_guess("a")
 
 
 

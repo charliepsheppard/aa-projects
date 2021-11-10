3f5c8588527b4c65d7ffcced1232b1bc7241e233
thepshay <perry.xie1@gmail.com>
2021-11-10 13:27:12 -0800

    deep dup:

--- recursion/recursion_exercises.rb
+++ recursion/recursion_exercises.rb
@@ -12,6 +12,47 @@ def exp_1(base, exp)
   base * exp_1(base, exp - 1)
 end
 
-p exp_1(5, 0)
-p exp_1(5, 3)
-p exp_1(2, 4)
+# p exp_1(5, 0)
+# p exp_1(5, 3)
+# p exp_1(2, 4)
+
+def exp_2(base, exp)
+  return 1 if exp == 0
+  if exp.even?
+    exp_2(base, exp / 2) ** 2
+  else
+    base * (exp_2(base, (exp - 1) / 2) ** 2)
+  end
+end
+
+# p exp_1(5, 0)
+# p exp_1(5, 3)
+# p exp_1(2, 4)
+
+class Array
+
+  def deep_dup
+    return self if self.length <= 1
+
+    dup_arr = []
+    self.each do |ele|
+      if ele.is_a?(Array)
+        dup_arr << ele.deep_dup
+      else
+        dup_arr += [ele]
+      end
+    end
+    dup_arr
+  end
+end
+
+# dup_arr = [1, [2], [3, [4]]]
+
+# dup_arr = [2]
+# [2].deep_dup
+
+# dup_arr = [3] << [4] => [3, [4]]
+# [3, [4]].deep_dup
+
+test = [1, [2], [3, [4]]]
+p test.deep_dup

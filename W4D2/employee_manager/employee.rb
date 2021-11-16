6177466d5cc948020353ff21de3bf3a987cc1977
Charlie Sheppard <sheppard.charlie24@gmail.com>
2021-11-16 14:19:53 -0800

    Add initialize for employee and manager

---
+++ W4D2/employee_manager/employee.rb
@@ -0,0 +1,11 @@
+class Employee
+
+  attr_reader :name, :title, :salary, :boss
+
+  def initialize(name, title, salary, boss = nil)
+    @name = name 
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
+end

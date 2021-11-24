d978e6bbd95d87f73980fb38e0fc9b1cc1adac55
thiago <17tawi@gmail.com>
2021-11-23 16:23:29 -0800

    test the data base

--- W5D2/AA_questions/questions_db.rb
+++ W5D2/AA_questions/questions_db.rb
@@ -6,8 +6,8 @@ class QuestionsDataBase < SQLite3::Database
     include Singleton 
 
     def initialize
-        super('questions_db.rb')
-        self.type_translations = true
+        super('questions.db')
+        self.type_translation = true
         self.results_as_hash = true 
     end
 end
@@ -20,6 +20,11 @@ class User
         @lname = ops['lname'] 
     end
 
+    def self.all
+        data = QuestionsDataBase.instance.execute('SELECT * FROM users')
+        data.map { |datum| User.new(datum)}
+    end
+    
     def self.find_by_id(user_id)
         user = QuestionsDataBase.instance.execute(<<-SQL, user_id)
             SELECT *
@@ -28,7 +33,7 @@ class User
         SQL
 
         return nil if user.nil?
-        User.new(user.first)
+        User.new(user.last)
     end
 
     def self.find_by_name(fname, lname)
@@ -43,17 +48,35 @@ class User
     end
 end
 
-# class Question
+class Question
 
-#     def initialize(ops)
+    def initialize(ops)
         
-#         @title = ops['title']
-#         @body = ops['body']
-#         @users_id = ops['users_id']
-#     end
+        @title = ops['title']
+        @body = ops['body']
+        @users_id = ops['users_id']
+    end
 
-#     def self.find_by_id(user_id)
-#         user = QuestionsDataBase.instance.execute('SELECT * FROM users WHERE id = user_id ')
-#     end
-# end
+    def self.find_by_user_id(user_id)
+        user = QuestionsDataBase.instance.execute(<<-SQL, user_id)
+            SELECT * 
+            FROM questions 
+            WHERE users_id = ?
+        SQL
+        
+        return nil if user.nil?
+        Question.new(user.first)
+    end
+
+    def self.find_by_athor_id(author_id)
+        question = QuestionsDataBase.instance.execute(<<-SQL, author_id)
+            SELECT *
+            FROM questions
+            WHERE id = ?
+        SQL
+
+        return nil if question.nil?
+        Question.new(question.first)
+    end
+end
 

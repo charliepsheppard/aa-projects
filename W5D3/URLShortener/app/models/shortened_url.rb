441ea389aa0c997aa047ba60763fd5c4ef5806c0
Charlie Sheppard <sheppard.charlie24@gmail.com>
2021-11-24 17:27:01 -0800

    Up to belongs_to and has_many in ShortenedURL

--- W5D3/URLShortener/app/models/shortened_url.rb
+++ W5D3/URLShortener/app/models/shortened_url.rb
@@ -8,6 +8,23 @@
 #  user_id   :integer          not null
 #
 class ShortenedURL < ApplicationRecord
-    validates :user_id, :long_url, presence: true, uniqueness: true 
-    validates :short_url
+    validates :user_id, :short_url, :long_url, presence: true, uniqueness: true 
+
+    belongs_to :submitter,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
+
+    def self.random_code 
+        short_url = SecureRandom.urlsafe_base64
+        unless ShortenedURL.exists?(short_url: short_url)
+            short_url = SecureRandom.urlsafe_base64
+        end
+
+        short_url
+    end
+
+    def create!(user, long_url)
+        ShortenedURL.random_code
+    end
 end
--- W5D3/URLShortener/db/schema.rb
+++ W5D3/URLShortener/db/schema.rb
@@ -16,10 +16,11 @@ ActiveRecord::Schema.define(version: 2021_11_24_233301) do
   enable_extension "plpgsql"
 
   create_table "shortened_urls", force: :cascade do |t|
-    t.string "short_url"
+    t.string "short_url", null: false
     t.string "long_url", null: false
     t.integer "user_id", null: false
     t.index ["short_url"], name: "index_shortened_urls_on_short_url"
+    t.index ["user_id"], name: "index_shortened_urls_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|

12ce7e47a9637689bc29627ea5a834c5a5e39b6d
Charlie Sheppard <sheppard.charlie24@gmail.com>
2021-11-30 17:04:14 -0800

    Up to sql_object

---
+++ W6D2/build_your_own_active_record/lib/01_sql_object.rb
@@ -0,0 +1,68 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    columns = DBConnection.execute2(<<-SQL)
+      SELECT
+        cats
+      FROM
+        INFORMATION_SCHEMA.TABLES
+      WHERE
+        TABLE_TYPE = 'BASE TABLE'
+      AND
+        TABLE_SCHEMA = 'cats';
+    SQL
+
+    p columns
+  end
+
+  def self.finalize!
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name || self.name.underscore.pluralize
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    # ...
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end

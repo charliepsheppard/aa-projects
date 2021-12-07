93da50b39fb4be346ade8a4f62effa8f65d82419
HelenEdwards <hgedwards87@gmail.com>
2021-12-06 16:01:17 -0800

    work on user controller and model

--- W7D1/99_cats_II/app/models/user.rb
+++ W7D1/99_cats_II/app/models/user.rb
@@ -3,19 +3,37 @@ class User < ApplicationRecord
   validates :password_digest, presence: true
   attr_reader :password
 
-  def reset_session_token!
-    self.session_token = SecureRandom::urlsafe_base64
-    self.save
-    self.session_token
-  end
-
-  def is_password?(password)
-    password_object = BCrypt::Password.new(password_digest)
-    password_object.is_password?(password)
+ def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
   end
 
   def password=(password)
     self.password_digest = BCrypt::Password.create(password)
     @password = password
   end
+
+
+  def is_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save
+    self.session_token
+  end
+
+
+  private
+
+  def ensure_session_token
+      self.session_token ||= SecureRandom::urlsafe_base64
+  end
+ 
 end

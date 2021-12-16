087044c83d0f131a226f96fdb2136d1208cdb9e9
zack alsiday <alsidayzack@gmail.com>
2021-12-15 16:06:13 -0800

    boards

--- Week8/Day3/reversi/src/board.js
+++ Week8/Day3/reversi/src/board.js
@@ -9,6 +9,11 @@ if (typeof window === 'undefined'){
  * and two white pieces at [3, 3] and [4, 4]
  */
 function _makeGrid () {
+    let newGrid = new Array(8);
+    for (let i = 0; i < newGrid.length; i++) {
+      newGrid[i] = new Array(8);
+    } 
+    return newGrid;
 }
 
 /**

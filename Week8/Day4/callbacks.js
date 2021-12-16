60877eb3a2c599c498c831249a3be3d53ede64b4
Charlie Sheppard <sheppard.charlie24@gmail.com>
2021-12-16 14:38:05 -0800

    Finish up to _tick

--- Week8/Day4/callbacks.js
+++ Week8/Day4/callbacks.js
@@ -4,16 +4,23 @@ class Clock {
     // 2. Store the hours, minutes, and seconds.
     // 3. Call printTime.
     // 4. Schedule the tick at 1 second intervals.
+    let date = new Date();
+    this.hour = date.getHours();
+    this.minute = date.getMinutes();
+    this.seconds = date.getSeconds();
+    this.printTime();
   }
 
   printTime() {
     // Format the time in HH:MM:SS
     // Use console.log to print it.
+    console.log(`${this.hour}:${this.minute}:${this.seconds}`);
   }
 
   _tick() {
     // 1. Increment the time by one second.
     // 2. Call printTime.
+    if (this.seconds )
   }
 }
 

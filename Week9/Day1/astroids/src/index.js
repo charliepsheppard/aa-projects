579bebfd3a93600cf7296152e796f585475b5cb2
JJZhang <92764807+jjzhang329@users.noreply.github.com>
2021-12-27 13:56:05 -0600

    updates

--- Week9/Day1/astroids/src/index.js
+++ Week9/Day1/astroids/src/index.js
@@ -2,11 +2,20 @@ import './asteroid';
 import './bullet';
 import './game';
 import './gameView';
-// import './movingObject';
+
 const MovingObject = require("./movingObject")
 import './ship';
 import './utils';
 
 // const MovingObject = require("./movingObject.js");
-console.log('this is index')
 window.MovingObject = MovingObject;
+// console.log('this is index')
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    let canvas = document.getElementById("game-canvas");
+    let ctx = canvas.getContext('2d');
+
+})
+
+
--- Week9/Day1/astroids/src/movingObject.js
+++ Week9/Day1/astroids/src/movingObject.js
@@ -5,12 +5,24 @@ function MovingObject(object) {
   this.color = object.color;
 }
 
-// const mo = new MovingObject({
-//   pos: [30, 30],
-//   vel: [10, 10],
-//   radius: 5,
-//   color: "#00FF00"
-// });
+
+
+MovingObject.prototype.draw = function(ctx){
+  ctx.beginPath();
+  ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true);
+  ctx.fillStyle = "gray";
+  ctx.fill();
+}
+
+
+const mo = new MovingObject({
+  pos: [30, 30],
+  vel: [10, 10],
+  radius: 5,
+  color: "#00FF00"
+});
+console.log("this is moving");
+
+// mo.draw(canvas);
 
 module.exports = MovingObject;
-
